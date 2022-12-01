007ce5a2217df0b59d9baa12bf4f2e2d2d4b9968
unknown <humayun.51959@gmail.com>
2022-12-02 00:50:53 +0530

    Create new
    des

---
+++ new.py
@@ -0,0 +1,22 @@
+from modulefinder import replacePackageMap
+
+
+print("hello world")
+print("MY name is humayun")
+name = "tim"
+print(name+"is my name")
+print(name, "is my name")
+print("hi\n how are you")
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.isupper())
+print(len(name))
+print(name.index("i"))
+name.lower()
+print(name.replace('m','t'))
+name.replace("m","t")
+print(name)
+print(78*6)
+print(max(4,2))
+print("hello")
