ae8f8bff3a962877592344f1e9d9f874411eb6bf
unknown <humayun.51959@gmail.com>
2022-12-03 11:50:39 +0530

    list done

---
+++ list.py
@@ -0,0 +1,16 @@
+fruit=['mango','apple','grapes','banana']
+print(fruit)
+print(fruit[0])
+print(fruit[1][1])
+print(fruit[1:2])
+print(type(fruit))
+fruit[0]="java"
+print(fruit)
+print(fruit[-1])
+print(len(fruit))
+eglist= ["humayun",3,True]
+print(eglist)
+print(type(eglist))
+print(type(eglist[0]))
+listeg=list(("nigiera",23,False))
+print(listeg)
---
+++ listMethods.py
@@ -0,0 +1,30 @@
+list1=[1,3,2,8,9,4,5,6]
+list1.sort()
+print(list1)
+many="something"
+list2=["hello","may",many]
+list1.extend(list2)
+print(list1)
+print(list2)
+list2.append('cherry')
+print(list2)
+list2.insert(1,"how many")
+print(list2)
+list2.remove(many)
+print(list2)
+list1.clear
+print(list1)
+print(list2.index('cherry'))
+print(list2.count("cherry"))
+print(list2.count("l"))
+list2.sort()
+list2.reverse()
+print(list2)
+list3=list2.copy()
+list3.pop()
+print("list 3 is ",list3)
+list3.pop(2)
+print("list 3 is ",list3)
+del list3[0]
+del list3
+print(list3)
