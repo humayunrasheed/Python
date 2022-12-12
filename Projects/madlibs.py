74cd27c2c35b66e0960c582f8a1f2321ec6d09df
unknown <humayun.51959@gmail.com>
2022-12-12 21:14:16 +0530

    ignore file added

---
+++ Projects/madlibs.py
@@ -0,0 +1,5 @@
+name=input("Enter the name:")
+apple=input("Enter the fruit:")
+superhero=input("Enter the super hero name:")
+madlibs =f"Hello {name} . Have you eaten {apple} if not you can't become {superhero}"
+print(madlibs)
