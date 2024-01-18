1dac731770002b1ac0349d0335529abb932f1cad
unknown <104017282+humayunrasheed@users.noreply.github.com>
2024-01-18 11:05:11 +0530

    Adding Notes

---
+++ notes/n1.py
@@ -0,0 +1,28 @@
+import keyword as k 
+k.kwlist 
+# gives list of key words
+
+# dec-10
+# b-2
+# oct-8
+# hexDec-16
+
+#binary to decimal
+int("1111",2)
+# octal to decimal
+int('45',8)
+# hexa to deciaml
+int('2a',16)
+# decimal to binary 
+bin(7)
+# decimal to octal
+oct(56)
+# decimal to hexa 
+hex(75)
+
+
+# Bit wise opperations
+# bitwise and
+5 & 9
+# bitwise or 
+4 | 5
