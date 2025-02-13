2f0565bf4a5d25ff10971c84cb4cc7d65ce1cc16
Syed Humayun Rasheed <104017282+humayunrasheed@users.noreply.github.com>
2025-02-13 13:18:02 +0530

    chapter 2 done

---
+++ Data_Analysis/chapter_2/ipython_script_test.py
@@ -0,0 +1,20 @@
+def f(x:int,y:int,z:int)-> int:
+    """  add first two numbers and divide by third number
+    
+
+    Args:
+        x (int): Integer input
+        y (int): Integer input
+        z (int): Integer input
+
+    Returns:
+        int: the sum of x and y divided by z
+    """
+    return (x+y)/z
+
+a = 5
+b = 6
+c = 7.5
+
+result = f(a,b,c)
+
